0a0550d2b182fe68ea8055dc21a4d6b77575e522
anotherFeng <regnagleppod@gmail.com>
2018-12-09 01:39:45 -0500

    Add Navbar component, add material icon dependency

--- freedom-for-data-client/src/App.js
+++ freedom-for-data-client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './shared/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar></Navbar>
         Test
       </div>
     );
